1b9536e4174d94a9b0277403ff0e2da4a5f7b717
Mark <menachemsalzberg13@gmail.com>
2025-10-12 14:41:00 +0300

    feat: restructure routing in App component to include Layout wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./styles/App.css";
 import { Routes, Route } from "react-router";
+import Layout from "./components/Layout";
 import SearchPage from "./pages/SearchPage";
 import ResultsPage from "./pages/ResultsPage";
 import FlightDetailsPage from "./pages/FlightDetailsPage";
@@ -9,10 +10,12 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<SearchPage />} />
-        <Route path="/results" element={<ResultsPage />} />
-        <Route path="/flight/:flightId" element={<FlightDetailsPage />} />
-        <Route path="/airport/:code" element={<AirportDetailsPage />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<SearchPage />} />
+          <Route path="results" element={<ResultsPage />} />
+          <Route path="flight/:flightId" element={<FlightDetailsPage />} />
+          <Route path="airport/:code" element={<AirportDetailsPage />} />
+        </Route>
       </Routes>
     </>
   );
