8e9907a09402677dd600280d73ab354a53ebfe65
Mark <menachemsalzberg13@gmail.com>
2025-10-12 14:15:47 +0300

    feat: add page components for Search, Results, Flight Details, and Airport Details

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import { Routes, Route } from "react-router";
 import "./App.css";
+import SearchPage from "./pages/SearchPage";
+import ResultsPage from "./pages/ResultsPage";
+import FlightDetailsPage from "./pages/FlightDetailsPage";
+import AirportDetailsPage from "./pages/AirportDetailsPage";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<SearchPage />} />
+        <Route path="/results" element={<ResultsPage />} />
+        <Route path="/flight/:flightId" element={<FlightDetailsPage />} />
+        <Route path="/airport/:code" element={<AirportDetailsPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
