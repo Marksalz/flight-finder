975e5806fa40854ad7c87c617cdd5846c52f35bd
Mark <menachemsalzberg13@gmail.com>
2025-10-15 16:11:06 +0300

    feat: implement ResultsPage component to display flight summary using SearchSummaryBar

--- src/pages/ResultsPage.jsx
+++ src/pages/ResultsPage.jsx
@@ -1,3 +1,9 @@
-export default function ResultsPage() {
-  return <></>;
+import SearchSummaryBar from "../components/SearchSummaryBar";
+
+export default function ResultsPage({ from, to, departDate, returnDate }) {
+  return (
+    <>
+      <SearchSummaryBar from={from} to={to} date={departDate} />
+    </>
+  );
 }
