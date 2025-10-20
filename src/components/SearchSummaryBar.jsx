80b2b32fdd813263edc0dae89580ea10e89cbe9a
Mark <menachemsalzberg13@gmail.com>
2025-10-20 11:55:48 +0300

    style: refactor SearchSummaryBar component layout and improve structure

--- src/components/SearchSummaryBar.jsx
+++ src/components/SearchSummaryBar.jsx
@@ -8,29 +8,67 @@ const formatDate = (dateStr) => {
 
 export default function SearchSummaryBar({ from, to, date }) {
   return (
-    <>
+    <Card
+      elevation={0}
+      sx={{
+        p: 2,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "flex-start",
+        gap: 2,
+        backgroundColor: "#cfdef3",
+        width: "100%",
+        borderRadius: 0,
+        border: 0,
+        boxShadow: "none",
+      }}
+    >
       <Card
-        elevation={0}
         sx={{
-          p: 2,
           display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
           alignItems: "center",
-          justifyContent: "flex-start",
-          gap: 2,
-          backgroundColor: "#cfdef3",
-          width: "100%",
-          borderRadius: 0,
-          border: 0,
-          boxShadow: "none",
+          bgcolor: "#9dc2f4ff",
+          borderRadius: 4,
+          padding: 2,
+          minWidth: "80%",
+          margin: "auto",
         }}
       >
-        <Typography variant="h6" component="div">
-          {from} &rarr; {to}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {formatDate(date)}
-        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: 2,
+          }}
+        >
+          <Typography variant="h4" component="div">
+            Flight Route:
+          </Typography>
+          <Typography variant="h5" component="div">
+            {from} &rarr; {to}
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: 2,
+          }}
+        >
+          <Typography variant="h4" component="div">
+            Date:
+          </Typography>
+          <Typography variant="h5" color="text.secondary">
+            {formatDate(date)}
+          </Typography>
+        </Box>
       </Card>
-    </>
+    </Card>
   );
 }
