02acabd5ddd663d256d05ef10568434794086bee
Mark <menachemsalzberg13@gmail.com>
2025-10-23 10:06:15 +0300

    feat: add mock flight data utility for testing purposes

--- src/features/flights/flightsSlice.js
+++ src/features/flights/flightsSlice.js
@@ -1,4 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { allFlights } from "../../utils/mockFlights";
 
 export const fetchFlights = createAsyncThunk(
   "flights/fetchFlights",
