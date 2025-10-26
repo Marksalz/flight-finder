5a964fda0a5ba6945e9cbf3be7dadd20f497fb4c
Mark <menachemsalzberg13@gmail.com>
2025-10-26 16:52:44 +0200

    feat: rename readById to fetchAirportById for consistency and clarity

--- src/utils/airportsApi.js
+++ src/utils/airportsApi.js
@@ -1,5 +1,8 @@
 import { allAirports } from "./mockAirports";
 
-export function readById(id) {
+export async function fetchAirportById(id) {
   return allAirports.filter((airport) => airport.id === id)[0];
+  // const response = await fetch(`/api/airports/${id}`);
+  // if (!response.ok) throw new Error("Network response was not ok");
+  // return await response.json();
 }
