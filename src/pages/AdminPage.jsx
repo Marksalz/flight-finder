1271c7fdc9afa0bffb6f87ca060e341962a42560
Mark <menachemsalzberg13@gmail.com>
2025-11-04 11:52:00 +0200

    refactor: remove unused clearFlights logic and streamline form submission in AdminPage

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -3,30 +3,20 @@ import AdminFilterForm from "../components/adminFliterForm";
 import { fetchFlights, clearFlights } from "../features/flights/flightsSlice";
 import FlightsList from "../components/FlightsList";
 import { useEffect } from "react";
+import { setAdminSearchParams } from "../features/search/searchSlice";
 
 export default function AdminPage() {
   const dispatch = useDispatch();
 
   const handeleSubmit = (formData) => {
-    console.log(formData);
+    dispatch(setAdminSearchParams(formData));
     dispatch(fetchFlights({ searchParams: formData }));
   };
-
-  const handleClear = () => {
-    dispatch(clearFlights());
-  };
-
-//   useEffect(() => {
-//     // Cleanup flights when leaving the page
-//     return () => {
-//       dispatch(clearFlights());
-//     };
-//   }, [dispatch]);
 
   const flights = useSelector((state) => state.flights.flights);
   return (
     <>
-      <AdminFilterForm handeleSubmit={handeleSubmit} onClear={handleClear} />
+      <AdminFilterForm handeleSubmit={handeleSubmit} />
       {flights && flights.length > 0 && <FlightsList flights={flights} />}
     </>
   );
