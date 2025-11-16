bc72e26b2b6cbce7a574d19e01fbc911563cae13
Mark <menachemsalzberg13@gmail.com>
2025-11-16 15:21:07 +0200

    refactor: clean up imports and simplify JSX structure in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
+import { Routes, Route } from "react-router";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+
 import { fetchAirports } from "./features/airports/airportsSlice";
 import FlightDetailsPage from "./pages/FlightDetailsPage";
 import ResultsPage from "./pages/ResultsPage";
 import SearchPage from "./pages/SearchPage";
 import AdminPage from "./pages/AdminPage";
 import Layout from "./components/Layout";
-
-import { Routes, Route } from "react-router";
-import { useDispatch } from "react-redux";
-import { useEffect } from "react";
 
 function App() {
   const dispatch = useDispatch();
@@ -17,16 +17,14 @@ function App() {
   }, [dispatch]);
 
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<SearchPage />} />
-          <Route path="results" element={<ResultsPage />} />
-          <Route path="flight/:flightId" element={<FlightDetailsPage />} />
-          <Route path="admin" element={<AdminPage />} />
-        </Route>
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<SearchPage />} />
+        <Route path="results" element={<ResultsPage />} />
+        <Route path="flight/:flightId" element={<FlightDetailsPage />} />
+        <Route path="admin" element={<AdminPage />} />
+      </Route>
+    </Routes>
   );
 }
 
