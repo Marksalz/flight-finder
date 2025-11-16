58c6546078f7e07033a37f7e03094d14e849929f
Mark <menachemsalzberg13@gmail.com>
2025-11-16 15:22:06 +0200

    fix: change import order

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+
 import airportsReducer from "../features/airports/airportsSlice";
 import flightsReducer from "../features/flights/flightsSlice";
 import searchReducer from "../features/search/searchSlice";
-import { configureStore } from "@reduxjs/toolkit";
 
 const store = configureStore({
   reducer: {
