9fc3ef5afd3eca5738a41c711422e7c977ac7864
Mark <menachemsalzberg13@gmail.com>
2025-11-23 16:47:58 +0200

    refactor: improve code structure and readability in FlightRouteMap component

--- src/components/flightDetailsPage/FlightRouteMap.jsx
+++ src/components/flightDetailsPage/FlightRouteMap.jsx
@@ -1,54 +1,54 @@
+import { useEffect } from "react";
+
 import { MapContainer, TileLayer, useMap } from "react-leaflet";
+import * as LeafletLib from "leaflet";
 import "leaflet/dist/leaflet.css";
 import "@elfalem/leaflet-curve";
-import L from "leaflet";
 
 import { Card } from "@mui/material";
-
-import { useEffect } from "react";
 
 function FlightRoute({ origin, destination }) {
   const map = useMap();
 
   useEffect(() => {
-    if (!origin || !destination) return;
+    if (origin && destination) {
+      let curve;
 
-    let curve;
+      const lngDiff = Math.abs(destination.lng - origin.lng);
+      const latDiff = Math.abs(destination.lat - origin.lat);
 
-    const lngDiff = Math.abs(destination.lng - origin.lng);
-    const latDiff = Math.abs(destination.lat - origin.lat);
+      const distance = Math.sqrt(lngDiff * lngDiff + latDiff * latDiff);
 
-    const distance = Math.sqrt(lngDiff * lngDiff + latDiff * latDiff);
+      const curveFactor = Math.min(Math.max(distance * 0.3, 2), 20);
 
-    const curveFactor = Math.min(Math.max(distance * 0.3, 2), 20);
+      const controlPoint = [
+        (origin.lat + destination.lat) / 2 + curveFactor,
+        (origin.lng + destination.lng) / 2,
+      ];
 
-    const controlPoint = [
-      (origin.lat + destination.lat) / 2 + curveFactor,
-      (origin.lng + destination.lng) / 2,
-    ];
+      const latlngs = [
+        "M",
+        [origin.lat, origin.lng],
+        "Q",
+        controlPoint,
+        [destination.lat, destination.lng],
+      ];
 
-    const latlngs = [
-      "M",
-      [origin.lat, origin.lng],
-      "Q",
-      controlPoint,
-      [destination.lat, destination.lng],
-    ];
+      curve = LeafletLib.curve(latlngs, {
+        color: "dodgerblue",
+        weight: 3,
+        dashArray: null,
+        smoothFactor: 1,
+      }).addTo(map);
 
-    curve = L.curve(latlngs, {
-      color: "dodgerblue",
-      weight: 3,
-      dashArray: null,
-      smoothFactor: 1,
-    }).addTo(map);
+      map.fitBounds([origin, destination], { padding: [50, 50] });
 
-    map.fitBounds([origin, destination], { padding: [50, 50] });
-
-    return () => {
-      if (curve) {
-        map.removeLayer(curve);
-      }
-    };
+      return () => {
+        if (curve) {
+          map.removeLayer(curve);
+        }
+      };
+    }
   }, [map, origin, destination]);
 
   return null;
