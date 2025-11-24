963a6665259ea0eadcd738428f4778778f0f3379
Mark <menachemsalzberg13@gmail.com>
2025-11-24 16:20:37 +0200

    refactor: reorder imports for better organization in SearchPage

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,6 +1,6 @@
-import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
 
 import dayjs from "dayjs";
 
