252f8544614b6c8bfa3a35c960b1583f03e96681
Mark <menachemsalzberg13@gmail.com>
2026-01-12 16:43:39 +0200

    feat: implement CRUD operations for flights in flight router

--- backend/routes/flightRoutes.js
+++ backend/routes/flightRoutes.js
@@ -44,4 +44,105 @@ flightRouter.get("/", async (req, res) => {
   }
 });
 
+flightRouter.get("/:flightId", async (req, res) => {
+  try {
+    const { flightId } = req.params;
+
+    const flight = flights.find((flight) => flight.id === flightId);
+
+    if (!flight) {
+      return res.status(404).json({
+        success: false,
+        message: "Flight not found",
+      });
+    }
+
+    res.json({ success: true, flight: flight });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      details: error.message,
+    });
+  }
+});
+
+flightRouter.post("/create", async (req, res) => {
+  try {
+    const flightData = req.body;
+
+    const newId = findNextId(flights);
+    const newFlight = { id: newId, ...flightData };
+    flights.push(newFlight);
+
+    await writeFile(
+      new URL("../data/db.json", import.meta.url),
+      JSON.stringify({ ...data, flights }, null, 2)
+    );
+    res.status(201).json({ success: true, flight: newFlight });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      details: error.message,
+    });
+  }
+});
+
+flightRouter.put("/:flightId", async (req, res) => {
+  try {
+    const { flightId } = req.params;
+    const flightData = req.body;
+
+    const flightIndex = flights.findIndex((flight) => flight.id === flightId);
+
+    if (flightIndex === -1) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Flight not found" });
+    }
+
+    const updatedFlight = { ...flights[flightIndex], ...flightData };
+    flights[flightIndex] = updatedFlight;
+
+    await writeFile(
+      new URL("../data/db.json", import.meta.url),
+      JSON.stringify({ ...data, flights }, null, 2)
+    );
+    res.json({ success: true, flight: updatedFlight });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      details: error.message,
+    });
+  }
+});
+
+flightRouter.delete("/:flightId", async (req, res) => {
+  try {
+    const { flightId } = req.params;
+
+    const flightToDelete = flights.find((flight) => flight.id === flightId);
+
+    if (!flightToDelete) {
+      return res.status(404).json({
+        success: false,
+        message: "Flight not found",
+      });
+    }
+
+    const updatedFlights = flights.filter((flight) => flight.id !== flightId);
+    data.flights = updatedFlights;
+
+    await writeFile(
+      new URL("../data/db.json", import.meta.url),
+      JSON.stringify(data, null, 2)
+    );
+    res.json({ success: true, deleted: flightToDelete });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      details: error.message,
+    });
+  }
+});
+
 export default flightRouter;
