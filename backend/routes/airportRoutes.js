01992412aee6908abd58815997d2268207ed3e1f
Mark <menachemsalzberg13@gmail.com>
2026-01-12 16:46:36 +0200

    feat: add delete route for airports and refactor ID generation function

--- backend/routes/airportRoutes.js
+++ backend/routes/airportRoutes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { readFile, writeFile } from "fs/promises";
-import { findNextAirportId } from "../utils/helperFunctions.js";
+import { findNextId } from "../utils/helperFunctions.js";
 
 const data = JSON.parse(
   await readFile(new URL("../data/db.json", import.meta.url))
 );
 
-const airports = data.airports;
+const { airports } = data;
 
 const airportRouter = Router();
 
@@ -46,7 +46,7 @@ airportRouter.post("/create", async (req, res) => {
   try {
     const airportData = req.body;
 
-    const newId = findNextAirportId(airports);
+    const newId = findNextId(airports);
     const newAirport = { id: newId, ...airportData };
     airports.push(newAirport);
 
@@ -64,29 +64,61 @@ airportRouter.post("/create", async (req, res) => {
 });
 
 airportRouter.put("/:airportId", async (req, res) => {
-  const { airportId } = req.params;
-  const airportData = req.body;
-
-  const airportIndex = airports.findIndex(
-    (airport) => airport.id === airportId
-  );
-
-  if (airportIndex === -1) {
-    return res
-      .status(404)
-      .json({ success: false, message: "Airport not found" });
-  }
-
-  const updatedAirport = { ...airports[airportIndex], ...airportData };
-  airports[airportIndex] = updatedAirport;
-
-  // Write updated airports to the JSON file
   try {
+    const { airportId } = req.params;
+    const airportData = req.body;
+
+    const airportIndex = airports.findIndex(
+      (airport) => airport.id === airportId
+    );
+
+    if (airportIndex === -1) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Airport not found" });
+    }
+
+    const updatedAirport = { ...airports[airportIndex], ...airportData };
+    airports[airportIndex] = updatedAirport;
+
     await writeFile(
       new URL("../data/db.json", import.meta.url),
       JSON.stringify({ ...data, airports }, null, 2)
     );
     res.json({ success: true, airport: updatedAirport });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      details: error.message,
+    });
+  }
+});
+
+airportRouter.delete("/airportId", async (req, res) => {
+  try {
+    const { airportId } = req.params;
+
+    const airportToDelete = airports.find(
+      (airport) => airport.id === airportId
+    );
+
+    if (!airportToDelete) {
+      return res.status(404).json({
+        success: false,
+        message: "Airport not found",
+      });
+    }
+
+    const updatedAirports = airports.filter(
+      (airport) => airport.id !== airportId
+    );
+    data.airports = updatedAirports;
+
+    await writeFile(
+      new URL("../data/db.json", import.meta.url),
+      JSON.stringify(data, null, 2)
+    );
+    res.json({ success: true, deleted: airportToDelete });
   } catch (error) {
     res.status(500).json({
       success: false,
